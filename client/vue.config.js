e18f3b1511172a4b5335c7b599c48f4df13bd81a
flxcp <96683372+flxcp@users.noreply.github.com>
2022-04-25 21:10:02 +0000

    Deploy

---
+++ client/vue.config.js
@@ -0,0 +1,12 @@
+const { defineConfig } = require("@vue/cli-service");
+module.exports = defineConfig({
+  transpileDependencies: true,
+  pwa: {
+    name: "Cards",
+    themeColor: "#e6e6fa",
+    msTileColor: "#e6e6fa",
+    appleMobileWebAppCapable: "yes",
+    appleMobileWebAppStatusBarStyle: "black",
+  },
+  publicPath: "/static/app/",
+});
